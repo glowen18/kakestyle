c25f644197afb8a420520eb758434860d39c9bf8
Gaye Lowenstein <glowen@hotmail.com>
2016-10-25 10:59:55 -0700

    adds RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  get 'garments/' => 'garments#index'
+
+  get 'garments/new' => 'garments#new', as: :new_garment
+
+  get 'garments/:id/' => 'garments#show', as: :garment
+
+  get 'garments/:id/edit' => 'garments#edit', as: :edit_garment
+
+  post 'garments' => 'garments#create'
+
+  patch 'garments/:id' => 'garments#update'
+
+  delete 'garments/:id' => 'garments#destroy'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
